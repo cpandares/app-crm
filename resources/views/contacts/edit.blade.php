4c452240c8703a0d01df2503c9018eda094586b1
cpandares <pandaresocesar@gmail.com>
2023-02-27 09:47:35 -0400

    fix

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -203,14 +203,13 @@
                                 <!--end::Header-->
                                 <!--begin::Body-->
                                 <div class="card-body pt-5">
-                                    <!--begin::Timeline-->
+                                   
                                     <div class="timeline-label">
-                                        <!--begin::Item-->
+                                       
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">08:42</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                           
                                             <div class="timeline-badge">
                                                 <a href="" title="Email">
                                                     <i class="fa fa-genderless text-warning fs-1">
@@ -218,134 +217,112 @@
                                                     </i>
                                                 </a>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Text-->
+                                          
                                             <div class="fw-mormal timeline-content text-muted ps-3">
-                                                <h2>Asunto:</h2>
+                                                <h2>Asunto: Incicio de negociacion</h2>
+                                                <h5>Contacto mediante: Email</h5>
                                                 <span>
                                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, placeat culpa accusantium delectus at eius labore necessitatibus dicta accusamus nisi hic! A autem, culpa itaque repellat dolore laboriosam excepturi ut impedit, laborum obcaecati asperiores ullam architecto voluptatibus. Nemo non illum cumque ad! Aut vero nam aspernatur vel tempore voluptatum sit.
                                                 </span>
                                             </div>
-                                            <!--end::Text-->
+                                            
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
-                                        <div class="timeline-item">
-                                            <!--begin::Label-->
+                                       
+                                        {{-- <div class="timeline-item">
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">10:00</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                           
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-success fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Content-->
+                                            
                                             <div class="timeline-content d-flex">
-                                                <span class="fw-bolder text-gray-800 ps-3">AEOL meeting</span>
+                                                <span class="fw-bolder text-gray-800 ps-3"></span>
                                             </div>
-                                            <!--end::Content-->
+                                            
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                        
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                           
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">14:37</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                          
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-danger fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Desc-->
-                                            <div class="timeline-content fw-bolder text-gray-800 ps-3">Make deposit
-                                            <a href="#" class="text-primary">USD 700</a>. to ESL</div>
-                                            <!--end::Desc-->
+                                            
+                                            <div class="timeline-content fw-bolder text-gray-800 ps-3">Envio de presupuesto
+                                            <a href="#" class="text-primary">USD 700</a>. </div>
+                                            
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                       
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">16:50</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                           
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-primary fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Text-->
+                                           
                                             <div class="timeline-content fw-mormal text-muted ps-3">Indulging in poorly driving and keep structure keep great</div>
-                                            <!--end::Text-->
+                                           
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                        
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">21:03</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                         
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-danger fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Desc-->
+                                          
                                             <div class="timeline-content fw-bold text-gray-800 ps-3">New order placed
                                             <a href="#" class="text-primary">#XF-2356</a>.</div>
-                                            <!--end::Desc-->
+                                            <
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                        
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">16:50</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                          
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-primary fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Text-->
+                                            
                                             <div class="timeline-content fw-mormal text-muted ps-3">Indulging in poorly driving and keep structure keep great</div>
-                                            <!--end::Text-->
+                                           
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                       
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">21:03</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                           
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-danger fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Desc-->
+                                           
                                             <div class="timeline-content fw-bold text-gray-800 ps-3">New order placed
                                             <a href="#" class="text-primary">#XF-2356</a>.</div>
-                                            <!--end::Desc-->
+                                            
                                         </div>
-                                        <!--end::Item-->
-                                        <!--begin::Item-->
+                                       
                                         <div class="timeline-item">
-                                            <!--begin::Label-->
+                                            
                                             <div class="timeline-label fw-bolder text-gray-800 fs-6">10:30</div>
-                                            <!--end::Label-->
-                                            <!--begin::Badge-->
+                                            
                                             <div class="timeline-badge">
                                                 <i class="fa fa-genderless text-success fs-1"></i>
                                             </div>
-                                            <!--end::Badge-->
-                                            <!--begin::Text-->
+                                           
                                             <div class="timeline-content fw-mormal text-muted ps-3">Finance KPI Mobile app launch preparion meeting</div>
-                                            <!--end::Text-->
-                                        </div>
-                                        <!--end::Item-->
+                                            
+                                        </div> --}}
+                                      
                                     </div>
-                                    <!--end::Timeline-->
+                                    
                                 </div>
-                                <!--end: Card Body-->
+                                
                             </div>
                         </div>
-                        <!-- /.tab-pane -->
                         <div class="tab-pane" id="campaings" >
                            
                             <table class="table table-striped mt-2">
