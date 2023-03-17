1fd2e066f651cf85d5f8c96aa6622d1fca648214
cpandares <pandaresocesar@gmail.com>
2023-03-17 10:47:38 -0400

    pedidos

--- resources/views/api/pedidos/index.blade.php
+++ resources/views/api/pedidos/index.blade.php
@@ -33,7 +33,7 @@
                                 </span>
                                 <!--end::Svg Icon-->
                                 <input type="text" data-kt-customer-table-filter="search"
-                                    class="form-control form-control-solid w-250px ps-15" placeholder="Search Customers">
+                                    class="form-control form-control-solid w-250px ps-15" placeholder="Buscar pedido">
                             </div>
                             <!--end::Search-->
                         </div>
