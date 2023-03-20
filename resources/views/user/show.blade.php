fd5677165a682d3e3ca5cda80e5d9b01a7c21ad7
cpandares <pandaresocesar@gmail.com>
2023-03-19 20:37:29 -0400

    fix profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -98,60 +98,7 @@
                     </div>
                     <!--end::Summary-->
                     <!--begin::Details toggle-->
-                    <div class="d-flex flex-stack fs-4 py-3">
-                        <div class="fw-bolder rotate collapsible" data-bs-toggle="collapse" href="#kt_customer_view_details" role="button" aria-expanded="false" aria-controls="kt_customer_view_details">Detalles
-                        <span class="ms-2 rotate-180">
-                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
-                            <span class="svg-icon svg-icon-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z" fill="black"></path>
-                                </svg>
-                            </span>
-                            <!--end::Svg Icon-->
-                        </span></div>
-                        <span data-bs-toggle="tooltip" data-bs-trigger="hover" title="" data-bs-original-title="Edit customer details">
-                            <a href="#" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_update_customer">Editar</a>
-                        </span>
-                    </div>
-                    <!--end::Details toggle-->
-                    <div class="separator separator-dashed my-3"></div>
-                    <!--begin::Details content-->
-                    <div id="kt_customer_view_details" class="collapse show">
-                        <div class="py-5 fs-6">
-                            <!--begin::Badge-->
-                            {{-- <div class="badge badge-light-info d-inline">Premium user</div> --}}
-                            <!--begin::Badge-->
-                            <!--begin::Details item-->
-                           {{--  <div class="fw-bolder mt-5">Account ID</div>
-                            <div class="text-gray-600">ID-45453423</div> --}}
-                            <!--begin::Details item-->
-                            <!--begin::Details item-->
-                            <div class="fw-bolder mt-5">Email</div>
-                            <div class="text-gray-600">
-                                <a href="#" class="text-gray-600 text-hover-primary">{{ $user->email }}</a>
-                            </div>
-                            <!--begin::Details item-->
-                            <!--begin::Details item-->
-                          {{--   <div class="fw-bolder mt-5">Dirección</div>
-                            <div class="text-gray-600">{{ $user->city }}, --}}
-                            {{-- <br>Melbourne 3000 VIC --}}
-                            {{-- <br>{{ $user->country }}</div> --}}
-                            <!--begin::Details item-->
-                            <!--begin::Details item-->
-                          {{--   <div class="fw-bolder mt-5">Language</div>
-                            <div class="text-gray-600">English</div> --}}
-                            <!--begin::Details item-->
-                            <!--begin::Details item-->
-                           {{--  <div class="fw-bolder mt-5">Upcoming Invoice</div>
-                            <div class="text-gray-600">54238-8693</div> --}}
-                            <!--begin::Details item-->
-                            <!--begin::Details item-->
-                           {{--  <div class="fw-bolder mt-5">Tax ID</div>
-                            <div class="text-gray-600">TX-8674</div> --}}
-                            <!--begin::Details item-->
-                        </div>
-                    </div>
-                    <!--end::Details content-->
+                   
                 </div>
                 <!--end::Card body-->
             </div>
